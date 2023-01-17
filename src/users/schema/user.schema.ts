e8b05ea0b8e933be69f1e21c13f5dd202b6ae880
Anis Dhaoui <freeconcept6@gmail.com>
2023-01-17 19:19:03 +0100

    Exclude user password from response

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -11,7 +11,7 @@ export class User {
     lastName: string;
     @Prop({ unique: true, required: true })
     email: string;
-    @Prop({ required: true })
+    @Prop({ required: true, select: false })
     password: string;
     @Prop({ required: true })
     userPic: string;
