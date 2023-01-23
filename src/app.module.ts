df19d5343a32016f2c9bde31a99363d70fcf7c9c
Anis Dhaoui <freeconcept6@gmail.com>
2023-01-23 14:08:56 +0100

    Geneating notification resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteModule } from './quote/quote.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AdminModule } from './admin/admin.module';
     QuoteModule,
     AuthModule,
     AdminModule,
+    NotificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
