c2669900f1423fc6e5a80c7f51f71983049977a6
Anis Dhaoui <freeconcept6@gmail.com>
2023-03-08 22:38:19 +0100

    Cleaning the code

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -5,82 +5,16 @@ import EntryCmp from './Entry/login-register.entry';
 import HomeCmp from './Home/quotes.home';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-// import { fetchQuotes } from '../state/actions-creators/quotes.actions-creators';
-// import { useAppDispatch, useAppSelector } from '../state/store.state';
 
 export default function Main() {
-    // const dispatch = useAppDispatch();
-    // const { quotes, loading, error } = useAppSelector(state => state.quotes);
-
-
-    // useEffect(() => {
-    //     dispatch(fetchQuotes())
-
-    // }, [dispatch])
-
-    // const renderQuotes = () => {
-    //     if (loading) {
-    //         return <p>Loading...</p>
-    //     }
-    //     if (error) {
-    //         return <p> {error} </p>
-    //     }
-    //     if (quotes && quotes.quotesData) {
-    //         const allQuotes: IQuote[] = quotes!.quotesData;
-    //         console.log(allQuotes)
-    //         return (
-    //             allQuotes.map(item => {
-    //                 return (
-    //                     <h3 key={item._id}> {item.author} </h3>
-    //                 )
-    //             })
-    //         )
-    //     }
-    // }
 
     return (
         <>
-            {/* <HeaderCmp /> */}
             <ToastContainer autoClose={3000} theme="colored" />
             <Routes>
                 <Route path='/' element={[<HeaderCmp key={1} />, <HomeCmp key={2} />]} />
                 <Route path='/entry' element={<EntryCmp />} />
             </Routes>
-
-            <div className='mt-5'>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-                <h1>aaaaaaaaaaaaaaaa</h1>
-            </div>
         </>
     )
 }
