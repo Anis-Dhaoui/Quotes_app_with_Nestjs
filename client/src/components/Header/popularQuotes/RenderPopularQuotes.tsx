870df140b3a55320aba0e67184c4fd6e00da7331
Anis Dhaoui Laptop <freeconcept6@gmail.com>
2023-12-26 23:45:28 +0100

    Commit before pull

--- client/src/components/Header/popularQuotes/RenderPopularQuotes.tsx
+++ client/src/components/Header/popularQuotes/RenderPopularQuotes.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import './style.popularQuotes.css';
+import './style.popularQuotes.scss';
 import { useAppDispatch, useAppSelector } from 'state/store.state';
 import { fetchPopularQuotes } from 'state/actions-creators/quotes.actions-creators';
 import Loader from 'shared/loader/loader';
@@ -29,20 +29,20 @@ export default function RenderPopularQuotes() {
     )
   }
 
-  const popularQuotesList = popularQuotesRes?.popularQuotes.map((item: any) => {
+  const popularQuotesList = popularQuotesRes?.popularQuotes.map((item: any, index: number) => {
     return (
-      <div key={item._id} className='carousel__item'>
-        <div className='carousel__item-head'>
-          <img src={img} alt='img' className='rounded-circle' width={80} height={80} />
-        </div>
-        <div className='carousel__item-body'>
-          <p style={{ fontWeight: "bold" }}>{item.author}</p>
-          <blockquote className="truncate-text" cite={item.author}>{item.quote}</blockquote>
-          <span style={{ fontSize: "8pt", position: "absolute", bottom: "0", right: "45%", left: "42%", marginBottom: "8px", opacity: "0.6" }}>{item.category}</span>
-        </div>
-        <div style={{ width: "100%", marginTop: "-4px" }}>
-          <InterractionBtns item={item} />
-        </div>
+      <div id={item._id} key={item._id} className='carousel__item'>
+          <div className='carousel__item-head'>
+            <img src={img} alt='img' className='rounded-circle' width={80} height={80} />
+          </div>
+          <div className='carousel__item-body'>
+            <p style={{ fontWeight: "bold" }}>{item.author}</p>
+            <blockquote className="truncate-text" cite={item.author}>{item.quote}</blockquote>
+            <span style={{ fontSize: "8pt", position: "absolute", bottom: "0", right: "45%", left: "42%", marginBottom: "8px", opacity: "0.6" }}>{item.category}</span>
+          </div>
+          <div style={{ width: "100%", marginTop: "-4px" }}>
+            <InterractionBtns item={item} />
+          </div>
       </div>
     )
   })
