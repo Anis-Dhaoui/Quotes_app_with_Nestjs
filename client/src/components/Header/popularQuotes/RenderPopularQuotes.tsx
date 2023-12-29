a2b32d0885d987fc555d4f62a37ce79a74da8e09
Anis Dhaoui <ussj@TN-WSW10-00454.benettongroup.org>
2023-12-29 17:05:51 +0100

    carousel popular quotes

--- client/src/components/Header/popularQuotes/RenderPopularQuotes.tsx
+++ client/src/components/Header/popularQuotes/RenderPopularQuotes.tsx
@@ -17,12 +17,26 @@ export default function RenderPopularQuotes() {
 
   const dispatch = useAppDispatch();
   const { popularQuotesReq, popularQuotesRes, popularQuotesErr } = useAppSelector(state => state.quotes);
+  const items = [
+    { id: 1, name: 'name1' },
+    { id: 2, name: 'name2' },
+    { id: 3, name: 'name3' },
+    { id: 4, name: 'name4' },
+    { id: 5, name: 'name5' },
+    { id: 6, name: 'name6' },
+    { id: 7, name: 'name7' },
+    { id: 8, name: 'name8' },
+  ];
 
   useEffect(() => {
     dispatch(fetchPopularQuotes());
   }, [dispatch])
 
-  console.log(popularQuotesRes)
+  console.log(`
+    REQ: ${popularQuotesReq} \n
+    RES: ${popularQuotesRes?.popularQuotes} \n
+    ERR: ${popularQuotesErr}
+  `)
 
   // State for Active index 
   const [activeIndex, setActiveIndex] = React.useState(0);
@@ -40,7 +54,6 @@ export default function RenderPopularQuotes() {
       itemLength : activeIndex - 1;
     setActiveIndex(nextIndex);
   }
-
   // Next button for Carousel 
   const nextButton = () => {
     if (animating) return;
@@ -49,38 +62,7 @@ export default function RenderPopularQuotes() {
     setActiveIndex(nextIndex);
   }
 
-  // Carousel Item Data 
-  const carouselItemData = popularQuotesRes?.popularQuotes.map((item: any) => {
-    return (
-      <CarouselItem
-        key={"item.src"}
-        onExited={() => setAnimating(false)}
-        onExiting={() => setAnimating(true)}
-      >
-        <div className="row">
-          {
-            [...Array(3)].map((_, index) => (
-              <div key={index} className="col-md-4 mb-3">
-                <div key={"item._id"} className='card_item'>
-                  <div className='card_item-head'>
-                    <img src={img} alt='img' className='rounded-circle' width={80} height={80} />
-                  </div>
-                  <div className='card_item-body'>
-                    <p style={{ fontWeight: "bold" }}>{"item.author"}</p>
-                    <blockquote className="truncate-text" cite={"item.author"}>{"item.quote"}</blockquote>
-                    <span style={{ fontSize: "8pt", position: "absolute", bottom: "0", right: "45%", left: "42%", marginBottom: "8px", opacity: "0.6" }}>{"item.category"}</span>
-                  </div>
-                  <div style={{ width: "100%", marginTop: "-2px" }}>
-                    {/* <InterractionBtns item={item} /> */}
-                  </div>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-      </CarouselItem>
-    );
-  });
+
 
   if (popularQuotesReq) {
     return <Loader />
@@ -94,40 +76,128 @@ export default function RenderPopularQuotes() {
     )
   }
 
+  // Chunk the items array into groups of 3 to create carousel items
+  const chunkedItems = items.reduce((resultArray: any, item: any, index: any) => {
+    const chunkIndex = Math.floor(index / 3);
 
-  // const popularQuotesList = popularQuotesRes?.popularQuotes.map((item: any) => {
-  const popularQuotesList = () => {
-    return (
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
+    }
 
-      <div>
-        <Carousel
-          previous={previousButton}
-          next={nextButton}
-          activeIndex={activeIndex}
-        >
-          {/* <CarouselIndicators
-            items={items}
-            activeIndex={activeIndex}
-            onClickHandler={(newIndex) => {
-              if (animating) return;
-              setActiveIndex(newIndex);
-            }}
-          /> */}
-          {carouselItemData}
-        </Carousel>
+    resultArray[chunkIndex].push(item);
+    return resultArray;
+  }, []);
+
+  // Carousel Item Data 
+  const carouselItems = chunkedItems.map((chunk: any, item: any) => (
+    <CarouselItem
+      key={item.id}
+      onExited={() => setAnimating(false)}
+      onExiting={() => setAnimating(true)}
+    >
+      <div className="row">
+        {
+          chunk.map((item: any, index: number) => (
+            <div key={index} className="col-md-4 mb-3">
+              <div className='card_item'>
+                <div className='card_item-head'>
+                  <img src={img} alt='img' className='rounded-circle' width={80} height={80} />
+                </div>
+                <div className='card_item-body'>
+                  <p style={{ fontWeight: "bold" }}>{item.name}</p>
+                  <blockquote className="truncate-text" cite={"item.author"}>{"item.quote"}</blockquote>
+                  <span style={{ fontSize: "8pt", position: "absolute", bottom: "0", right: "45%", left: "42%", marginBottom: "8px", opacity: "0.6" }}>{"item.category"}</span>
+                </div>
+                <div style={{ width: "100%", marginTop: "-2px" }}>
+                  {/* <InterractionBtns item={item} /> */}
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
-    )
-  }
+    </CarouselItem>
+  ));
 
   return (
-    <>
-      {popularQuotesList}
-    </>
-    // <div className='wrapper'>
-    //   <div className='carousel'>
-    //     {popularQuotesList()}
-    //   </div>
-    // </div>
-
+    <div>
+      <Carousel
+        previous={previousButton}
+        next={nextButton}
+        activeIndex={activeIndex}
+      >
+        {carouselItems.length > 0 ? carouselItems : <p>No items to display</p>}
+      </Carousel>
+    </div>
   )
 }
+
+
+
+
+
+// export default function RenderPopularQuotes() {
+
+//   const dispatch = useAppDispatch();
+//   const { popularQuotesReq, popularQuotesRes, popularQuotesErr } = useAppSelector(state => state.quotes);
+
+//   const items = [
+//     { id: 1, name: 'name1' },
+//     { id: 2, name: 'name2' },
+//     { id: 3, name: 'name3' },
+//     { id: 4, name: 'name4' },
+//     { id: 5, name: 'name5' },
+//     { id: 6, name: 'name6' },
+//     { id: 7, name: 'name7' },
+//     { id: 8, name: 'name8' },
+//   ];
+
+//   // Chunk the items array into groups of 3 to create carousel items
+//   const chunkedItems = items.reduce((resultArray: any, item: any, index: any) => {
+//     const chunkIndex = Math.floor(index / 3);
+
+//     if (!resultArray[chunkIndex]) {
+//       resultArray[chunkIndex] = []; // start a new chunk
+//     }
+
+//     resultArray[chunkIndex].push(item);
+//     return resultArray;
+//   }, []);
+
+//   // Map through the chunked items array and generate the carousel items
+//   const carouselItems = chunkedItems.map((chunk: any, index: any) => (
+//     <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+//       <div className="row">
+//         {chunk.map((item: any) => (
+//           <div key={index} className="col-md-4 mb-3">
+//             <div key={item.id} className='card_item'>
+//               <div className='card_item-head'>
+//                 <img src={img} alt='img' className='rounded-circle' width={80} height={80} />
+//               </div>
+//               <div className='card_item-body'>
+//                 <p style={{ fontWeight: "bold" }}>{item.author}</p>
+//                 <blockquote className="truncate-text" cite={item.author}>{item.quote}</blockquote>
+//                 <span style={{ fontSize: "8pt", position: "absolute", bottom: "0", right: "45%", left: "42%", marginBottom: "8px", opacity: "0.6" }}>{item.category}</span>
+//               </div>
+//               <div style={{ width: "100%", marginTop: "-2px" }}>
+//                 {/* <InterractionBtns item={item} /> */}
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   ));
+
+
+//   return (
+//     <div className="col-12">
+//       <div id="carouselExampleIndicators2" className="carousel slide" data-ride="carousel">
+//         <div className="carousel-inner">
+//           {carouselItems.length > 0 ? carouselItems : <p>No items to display</p>}
+//         </div>
+//       </div>
+//     </div>
+//   );
+
+// }
