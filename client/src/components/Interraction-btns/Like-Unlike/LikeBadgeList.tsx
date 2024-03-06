2307e8501b7e163bff16d5f154d9b1ebb6a7152a
Anis Benetton <freeconcept6@gmail.com>
2024-03-06 17:14:23 +0100

    Preparing post likes list

--- client/src/components/Interraction-btns/Like-Unlike/LikeBadgeList.tsx
+++ client/src/components/Interraction-btns/Like-Unlike/LikeBadgeList.tsx
@@ -4,9 +4,31 @@ function LikeBadgeList(props: any) {
     console.log(props.item)
     return (
         <>
-            <span className="badge"> {props.item?.likedBy.length} </span>
+            <span className="badge"> {props.item?.likedBy.length}
+                <div className="list-wrapper">
+                    <ul className="list">
+                        <li className="list-item">
+                            <div>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                            </div>
+                            <div className="list-item-content">
+                                <h4>Hitesh Kumar</h4>
+                            </div>
+                        </li>
+                        <li className="list-item">
+                            <div>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                            </div>
+                            <div className="list-item-content">
+                                <h4>Hitesh Kumar</h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </span>
 
-            <div className=""></div>
+
+
         </>
     )
 }
