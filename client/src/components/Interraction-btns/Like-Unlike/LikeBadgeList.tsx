cc4c78d53a2d68c68f415e9d5daea8f922308536
Anis Benetton <freeconcept6@gmail.com>
2024-03-08 15:02:47 +0100

    Post likes list Ready to receive real data

--- client/src/components/Interraction-btns/Like-Unlike/LikeBadgeList.tsx
+++ client/src/components/Interraction-btns/Like-Unlike/LikeBadgeList.tsx
@@ -1,95 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LikeBadgeList(props: any) {
+    const [showLikesList, setShowLikesList] = useState(false)
     console.log(props.item)
     return (
         <>
-            <span className="badge"> {props.item?.likedBy.length}
-                <div className="list-wrapper">
-                    <ul className="list">
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumarxxxxxxxxx</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div>
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
-                            </div>
-                            <div className="list-item-content">
-                                <h4>Hitesh Kumar</h4>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </span>
+            <span style={{cursor: "help"}} className="badge" onClick={() => setShowLikesList(!showLikesList)}> {props.item?.likedBy.length} </span>
+            <div className="list-wrapper" style={!showLikesList ? {display: "none"} : {display: "block"} }>
+                <ul className="list">
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumarxxxxxxxxx</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg" className="list-item-image" />
+                        </div>
+                        <div className="list-item-content">
+                            <h4>Hitesh Kumar</h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
 
 
 
